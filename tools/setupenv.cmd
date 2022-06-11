3f8bff483b2b9e0e0c15c328b416115faf636f98
Gian Borcillo <borcillofg2020@gmail.com>
2022-06-11 12:29:53 +0800

    feat: Remove appdirs module

--- tools/setupenv.cmd
+++ tools/setupenv.cmd
@@ -12,7 +12,7 @@ if errorlevel 1 goto pipCheckFailed
 
 :: Install required modules
 echo Install: Required Modules
-pip install pypresence appdirs
+pip install pypresence
 
 :: Then exit the script
 echo Done
